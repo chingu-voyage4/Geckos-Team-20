a915f71ae45b1992c5dd39188678a818f8707a24
DarisCalinor <candogan.tuzcu@gmail.com>
2018-03-25 18:10:47 +1100

    Refactor 'server.js' according to linter rules
    Refactor has been made according to AirBnB base rules. Details:
    
    1. All 'var' keywords have been replaced with 'const' keywords.
         Linter rule: no-var -all var keywords changed to const keyword
         https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
    2. String concatanes have been replaced with Template Literals.
         Linter rule: prefer-template -using string template feature from ES2015
         https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
    3. All callback functions have been rewritten as arrow functions.
         Linter rule: func-name + prefer-arrow-callback -always name your functions and use arrow
    functions when writing callback functions.
    https://stackoverflow.com/questions/22939130/when-should-i-use-arrow-functions-in-ecmascript-6
    4. Console statements have not been refactored. Before production phase they can be changed.

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-/* eslint-disable */
-var express = require("express");
-var app = express();
+const express = require("express");
 
-var path = process.cwd();
+const app = express();
+const port = process.env.PORT || 8080;
+const path = process.cwd();
 
-app.use(express.static(path + "/app/public"));
-app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist/"));
+app.use(express.static(`${path}/app/public`));
+app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist/`));
 app.use(
   "/bootstrap",
-  express.static(__dirname + "/node_modules/bootstrap/dist/")
+  express.static(`${__dirname}/node_modules/bootstrap/dist/`)
 );
 
 // Root Route
-app.get("/", function(req, res) {
-  //res.sendFile(path.join(__dirname,'public', 'index.html'));
-  res.sendFile(path + "/public/index.html");
-});
+app.get("/", (req, res) => res.sendFile(`${path}/public/index.html`));
 
 // Page Not Found Route
-app.get("*", function(req, res) {
-  res.sendFile(path + "/app/public/notfound.html");
-});
+app.get("*", (req, res) => res.sendFile(`${path}/app/public/notfound.html`));
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Node.js listening on port " + port + "...");
-});
+app.listen(port, () => console.log(`Node.js listening on port ${port}...`));
+
+/*
+no-var rules: all var keywords changed to const keyword
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+prefer-template: using string template feature from ES2015
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+func-name + prefer-arrow-callback: always name your functions and use arrow functions when writing
+  callback functions
+  https://stackoverflow.com/questions/22939130/when-should-i-use-arrow-functions-in-ecmascript-6
+no-console: We can leave console statements for a while. When we are going to production, we need to
+  figure out logging into a file placed in the server.
+*/
